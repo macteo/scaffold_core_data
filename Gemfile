1ff96b909547cda55385d6f069c2f4decb957978
Matteo Gavagnin <matteo.gavagnin@dimension.it>
2015-09-21 17:02:53 +0200

    Embrional model generator

---
+++ Gemfile
@@ -0,0 +1,4 @@
+gem "core_data", :git => "git@github.com:macteo/core_data.git"
+
+
+# '~/Dropbox/Indie/Pingu/PinguModel/Model.xcdatamodeld/Model 4.xcdatamodel'
---
+++ model_generator.rb
@@ -0,0 +1,38 @@
+require 'core_data'
+
+path = ARGV.first
+
+# path = '../PinguModel/Model.xcdatamodeld/Model 4.xcdatamodel'
+model = ::CoreData::DataModel.new(path)
+
+puts "\n"
+
+model.entities.each do |entity|
+  string = "rails destroy scaffold #{entity.name} -f -q\n"
+
+  string += "rails g scaffold #{entity.name} "
+
+  entity.attributes.each do |attribute|
+    type = "#{attribute.type}"
+    if (type == "Integer 16")
+      type = "integer"
+    end
+    if (type == "Double")
+      type = "float"
+    end
+    string += "#{attribute.name.downcase}:#{type.downcase} "
+  end
+
+  entity.relationships.each do |relation|
+    string += "#{relation.destination.downcase}:references "
+  end
+
+  string += "-f -q"
+  string += "\n"
+
+  puts string
+end
+
+puts "rake db:drop\n"
+puts "rake db:create\n"
+puts "rake db:migrate\n\n"
